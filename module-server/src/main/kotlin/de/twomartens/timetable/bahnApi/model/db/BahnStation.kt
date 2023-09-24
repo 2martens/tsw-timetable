bcbb935ac1e90c21281ba578a637fb1f879dbfe3
Jim Martens <github@2martens.de>
2023-09-24 09:01:34 +0200

    Changed to text index for name

--- module-server/src/main/kotlin/de/twomartens/timetable/bahnApi/model/db/BahnStation.kt
+++ module-server/src/main/kotlin/de/twomartens/timetable/bahnApi/model/db/BahnStation.kt
@@ -5,12 +5,13 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.index.TextIndexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
 @Document
 data class BahnStation(
-        @Indexed var name: String,
+        @TextIndexed var name: String,
         @Indexed(unique = true) var eva: String,
         var ds100: String,
         var db: Boolean
