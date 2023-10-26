f7de1c2825daff4082305bcf7ccbb9c0f89c44d6
Jim Martens <github@2martens.de>
2023-10-26 20:54:50 +0200

    Added properties to configure country names

---
+++ module-server/src/main/kotlin/de/twomartens/timetable/model/common/LanguageCode.kt
@@ -0,0 +1,6 @@
+package de.twomartens.timetable.model.common
+
+enum class LanguageCode {
+    de,
+    en
+}
--- module-server/src/main/kotlin/de/twomartens/timetable/property/ServiceProperties.kt
+++ module-server/src/main/kotlin/de/twomartens/timetable/property/ServiceProperties.kt
@@ -1,5 +1,8 @@
 package de.twomartens.timetable.property
 
+import de.twomartens.timetable.model.common.CountryCode
+import de.twomartens.timetable.model.common.LanguageCode
+import de.twomartens.timetable.types.NonEmptyString
 import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.cloud.context.config.annotation.RefreshScope
@@ -9,4 +12,5 @@ import org.springframework.cloud.context.config.annotation.RefreshScope
 @Schema(description = "Properties, to configure this Application")
 class ServiceProperties {
     lateinit var greeting: String
+    lateinit var countryNames: Map<CountryCode, Map<LanguageCode, NonEmptyString>>
 }
