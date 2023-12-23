988c3fa7aba7b101783437f448cd79a011a52955
Jim Martens <github@2martens.de>
2023-12-23 10:14:00 +0100

    change: Add index to userId

--- module-model/src/main/kotlin/de/twomartens/timetable/model/db/User.kt
+++ module-model/src/main/kotlin/de/twomartens/timetable/model/db/User.kt
@@ -7,12 +7,13 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
 @Document
 data class User(
-        var userId: UserId,
+        @Indexed(unique = true) var userId: UserId,
         var name: NonEmptyString,
         var email: Email
 ) {
