57baaa2b9147a4cb50e158a96be9f4fd6a5c8917
Jim Martens <github@2martens.de>
2023-12-24 11:23:17 +0100

    change: Only lookup after limiting result

--- module-model/src/main/kotlin/de/twomartens/timetable/model/repository/StationRepository.kt
+++ module-model/src/main/kotlin/de/twomartens/timetable/model/repository/StationRepository.kt
@@ -11,14 +11,20 @@ interface StationRepository : MongoRepository<Station, ObjectId> {
     fun findAllByCountryCode(countryCode: String): List<Station>
 
     @Aggregation(pipeline = [
-        "{\$search: { index: \"stations\",compound: {must: [{phrase: {query: ?0, path: \"countryCode\"}},{autocomplete: {query: ?1,path: \"name\",tokenOrder: \"sequential\"}}]}}}",
-        "{\$limit: 10}"
+        "{\$search: { index: \"stations\", returnStoredSource: true, compound: {must: [{phrase: {query: ?0, path: \"countryCode\"}},{autocomplete: {query: ?1,path: \"name\",tokenOrder: \"sequential\"}}]}}}",
+        "{\$limit: 10}",
+        "{\$lookup: { from: \"station\", localField: \"_id\", foreignField: \"_id\", as: \"document\" }}",
+        "{\$unwind: \"\$document\"}",
+        "{\$replaceWith: \"\$document\"}"
     ])
     fun findAllByCountryCodeAndNameContainingIgnoreCase(countryCode: String, name: String): List<Station>
 
     @Aggregation(pipeline = [
-        "{\$search: { index: \"stations\",autocomplete: {query: ?0, path: \"name\",tokenOrder: \"sequential\"}}}",
-        "{\$limit: 10}"
+        "{\$search: { index: \"stations\", returnStoredSource: true, autocomplete: {query: ?0, path: \"name\",tokenOrder: \"sequential\"}}}",
+        "{\$limit: 10}",
+        "{\$lookup: { from: \"station\", localField: \"_id\", foreignField: \"_id\", as: \"document\" }}",
+        "{\$unwind: \"\$document\"}",
+        "{\$replaceWith: \"\$document\"}"
     ])
     fun findAllByNameContainingIgnoreCase(name: String): List<Station>
 }
