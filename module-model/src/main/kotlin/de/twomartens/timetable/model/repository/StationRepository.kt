5f46334171447cf75ae50a0b0f6393b27308d78b
Jim Martens <github@2martens.de>
2023-12-24 09:43:17 +0100

    change: Use Atlas search for full-text search

--- module-model/src/main/kotlin/de/twomartens/timetable/model/repository/StationRepository.kt
+++ module-model/src/main/kotlin/de/twomartens/timetable/model/repository/StationRepository.kt
@@ -3,11 +3,20 @@ package de.twomartens.timetable.model.repository
 import de.twomartens.timetable.model.common.StationId
 import de.twomartens.timetable.model.db.Station
 import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface StationRepository : MongoRepository<Station, ObjectId> {
     fun findByCountryCodeAndStationId(countryCode: String, stationId: StationId): Station?
     fun findAllByCountryCode(countryCode: String): List<Station>
+
+    @Aggregation(pipeline = [
+        "{\$search: { index: \"stations\",compound: {must: [{phrase: {query: ?0, path: \"countryCode\"}},{autocomplete: {query: ?1,path: \"name\",tokenOrder: \"sequential\"}}]}}}"
+    ])
     fun findAllByCountryCodeAndNameContainingIgnoreCase(countryCode: String, name: String): List<Station>
+
+    @Aggregation(pipeline = [
+        "{\$search: { index: \"stations\",autocomplete: {query: ?0, path: \"name\",tokenOrder: \"sequential\"}}}"
+    ])
     fun findAllByNameContainingIgnoreCase(name: String): List<Station>
 }
