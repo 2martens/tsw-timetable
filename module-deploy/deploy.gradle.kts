30d5dc1f3d6ae0048f9c4d6033ac31cc7e8641a1
Jim Martens <github@2martens.de>
2025-10-03 16:51:39 +0200

    fix: Use Amazon mirror to download base image

--- module-deploy/deploy.gradle.kts
+++ module-deploy/deploy.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
 
 jib {
     from {
-        image = "amazoncorretto:" + properties["projectSourceCompatibility"] + "-alpine"
+        image = "public.ecr.aws/amazoncorretto:" + properties["projectSourceCompatibility"] + "-alpine"
         platforms {
             platform {
                 architecture = "amd64"
